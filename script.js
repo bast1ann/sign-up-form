6e021b93f1bbe3786f92cd30bb77e797c6272472
bast1ann <163662587+bast1ann@users.noreply.github.com>
2024-05-20 19:55:40 -0400

    Add styling when passwords do not match

---
+++ script.js
@@ -0,0 +1,19 @@
+const passwords = document.querySelectorAll("input[type='password']");
+const passwordOne = document.querySelector("#password");
+const passwordTwo = document.querySelector("#confirm-password");
+
+passwords.forEach( (item) => item.addEventListener("keyup", function() {
+  passwordsMatch(passwordOne, passwordTwo) ? 
+  passwordOne.className = "valid" : 
+  passwordOne.className = "invalid";
+}) );
+
+function passwordsMatch(pass1, pass2) {
+  if (pass1.value == pass2.value) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
